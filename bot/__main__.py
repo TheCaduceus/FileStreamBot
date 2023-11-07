e5525f17041d61f0943e0e114ce838e9bddb5845
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2023-11-07 14:39:08 +0530

    Bump to v1.1

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,7 @@
 from bot import TelegramBot, logger
-from bot.server import server
+from bot.server import run_server
 
 if __name__ == '__main__':
     logger.info('Initializing...')
-    TelegramBot.loop.create_task(server.serve())
+    TelegramBot.loop.create_task(run_server())
     TelegramBot.run()
