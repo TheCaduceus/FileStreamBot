08f48ebb72c98247952d0d356f24b1ae07b01aff
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2023-11-13 11:50:08 +0530

    Bump to v1.5

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,19 +1,15 @@
-from pyrogram import Client
+from telethon import TelegramClient
 from logging import getLogger
 from logging.config import dictConfig
 from .config import Telegram, LOGGER_CONFIG_JSON
 
 dictConfig(LOGGER_CONFIG_JSON)
 
-version = 1.4
+version = 1.2
 logger = getLogger('bot')
 
-TelegramBot = Client(
-    name="bot",
-    api_id = Telegram.API_ID,
-    api_hash = Telegram.API_HASH,
-    bot_token = Telegram.BOT_TOKEN,
-    plugins={"root": "bot/plugins"},
-    workers = Telegram.BOT_WORKERS,
-    max_concurrent_transmissions=1000
+TelegramBot = TelegramClient(
+    session='bot',
+    api_id=Telegram.API_ID,
+    api_hash=Telegram.API_HASH
 )
