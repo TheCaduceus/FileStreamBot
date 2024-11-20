679e3de021845e6ccffebba5922f0ddfa28c9f62
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-11-20 15:06:26 +0530

    Bump to v1.7

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,28 +1,18 @@
-from telethon import Button
-from telethon.events import NewMessage
-from telethon.tl.custom.message import Message
+from hydrogram import filters
+from hydrogram.types import Message
 from bot import TelegramBot
-from bot.config import Telegram
 from bot.modules.static import *
 from bot.modules.decorators import verify_user
 
-@TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start$'))
-@verify_user(private=True)
-async def welcome(event: NewMessage.Event | Message):
-    await event.reply(
-        message=WelcomeText % {'first_name': event.sender.first_name},
-        buttons=[
-            [
-                Button.url('Add to Channel', f'https://t.me/{Telegram.BOT_USERNAME}?startchannel&admin=post_messages+edit_messages+delete_messages')
-            ]
-        ]
+@TelegramBot.on_message(filters.command(['start', 'help']) & filters.private)
+@verify_user
+async def start_command(_, msg: Message):
+    await msg.reply(
+        text=WelcomeText % {'first_name': msg.from_user.first_name},
+        quote=True
     )
 
-@TelegramBot.on(NewMessage(incoming=True, pattern=r'^/info$'))
-@verify_user(private=True)
-async def user_info(event: Message):
-    await event.reply(UserInfoText.format(sender=event.sender))
-
-@TelegramBot.on(NewMessage(chats=Telegram.OWNER_ID, incoming=True, pattern=r'^/log$'))
-async def send_log(event: NewMessage.Event | Message):
-    await event.reply(file='event-log.txt')
+@TelegramBot.on_message(filters.command('privacy') & filters.private)
+@verify_user
+async def privacy_command(_, msg: Message):
+    await msg.reply(text=PrivacyText, quote=True)
