042a56ca7b5f755a527d904f96a23925057fd5f9
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-11-20 15:02:36 +0530

    Bump to v1.7

--- bot/modules/decorators.py
+++ bot/modules/decorators.py
@@ -1,20 +1,16 @@
-from telethon.events import NewMessage, CallbackQuery
-from typing import Callable
+from hydrogram import Client 
+from hydrogram.types import Message, CallbackQuery
+from typing import Union, Callable
 from functools import wraps
 from bot.config import Telegram
 
-def verify_user(private: bool = False):
-    
-    def decorator(func: Callable):
-        @wraps(func)
-        async def wrapper(update: NewMessage.Event | CallbackQuery.Event):
-            if private and not update.is_private:
-                return
+def verify_user(func: Callable):
 
-            chat_id = str(update.chat_id)
+    @wraps(func)
+    async def decorator(client: Client, update: Union[Message, CallbackQuery]):
+        chat_id = str(update.from_user.id if update.from_user else update.chat.id)
 
-            if not Telegram.ALLOWED_USER_IDS or chat_id in Telegram.ALLOWED_USER_IDS:
-                return await func(update)
-
-        return wrapper
+        if not Telegram.ALLOWED_USER_IDS or chat_id in Telegram.ALLOWED_USER_IDS:
+            return await func(client, update)
+        
     return decorator
