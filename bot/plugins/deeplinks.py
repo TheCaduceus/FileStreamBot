3e0fd905bb7866ef47f684cb6c8dfbf6ac04bfc7
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-11-20 15:07:10 +0530

    Bump to v1.7

--- bot/plugins/deeplinks.py
+++ bot/plugins/deeplinks.py
@@ -1,24 +1,25 @@
-from telethon.events import NewMessage
-from telethon.tl.custom import Message
-from bot import TelegramBot
-from bot.modules.decorators import verify_user
-from bot.modules.telegram import get_message, send_message
-from bot.modules.static import *
+# Currently not in use.
 
-@TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start file_'))
-@verify_user(private=True)
-async def send_file(event: NewMessage.Event | Message):
-    payload = event.raw_text.split()[-1].split('_')
+# from hydrogram.types import Message
+# from bot import TelegramBot
+# from bot.modules.decorators import verify_user
+# from bot.modules.telegram import get_message, send_message
+# from bot.modules.static import *
 
-    if len(payload) != 3:
-        return await event.reply(InvalidPayloadText)
-    
-    message = await get_message(int(payload[1]))
+# async def deeplinks(msg: Message, payload: str):
+#     if payload.startswith('file_'):
+#         sp = payload.split('_')
 
-    if not message:
-        return await event.reply(MessageNotExist)
-    if payload[2] != message.raw_text:
-        return await event.reply(InvalidPayloadText)
-    
-    message.raw_text = ''
-    await send_message(message, send_to=event.chat_id)
+#         if len(sp) != 3:
+#             return await msg.reply(InvalidPayloadText, quote=True)
+        
+#         message = await get_message(int(sp[1]))
+        
+#         if not message:
+#             return await msg.reply(MessageNotExist)
+#         if sp[2] != message.caption:
+#             return await msg.reply(InvalidPayloadText, quote=True)
+
+#         await message.copy(chat_id=msg.from_user.id, caption="")
+#     else:
+#         await msg.reply(InvalidPayloadText, quote=True)
