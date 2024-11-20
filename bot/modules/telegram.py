12bbc01f96374d6ca2d24b3e7898f3dda850ae0f
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-11-20 15:04:17 +0530

    Bump to v1.7

--- bot/modules/telegram.py
+++ bot/modules/telegram.py
@@ -1,5 +1,4 @@
-from telethon.events import NewMessage
-from telethon.tl.custom import Message
+from hydrogram.types import Message
 from datetime import datetime
 from mimetypes import guess_type
 from bot import TelegramBot
@@ -10,55 +9,47 @@ async def get_message(message_id: int) -> Message | None:
     message = None
     
     try:
-        message = await TelegramBot.get_messages(Telegram.CHANNEL_ID, ids=message_id)
+        message = await TelegramBot.get_messages(Telegram.CHANNEL_ID, message_ids=message_id)
+        if message.empty: message = None
     except Exception:
         pass
 
     return message
 
-async def send_message(message:Message, send_to:int = Telegram.CHANNEL_ID) -> Message:
-    return await TelegramBot.send_message(entity=send_to, message=message)
+async def send_message(msg: Message, send_to: int = Telegram.CHANNEL_ID) -> Message:
+    return await TelegramBot.send_message(entity=send_to, message=msg)
 
-def filter_files(update: NewMessage.Event | Message):
-    return bool(
-        (
-            update.document
-            or update.photo
-            or update.video
-            or update.video_note
-            or update.audio
-            or update.gif
-        )
-        and not update.sticker
+def get_file_properties(msg: Message):
+    attributes = (
+        'document',
+        'video',
+        'audio',
+        'voice',
+        'photo',
+        'video_note'
     )
+    for attribute in attributes:
+        media = getattr(msg, attribute, None)
+        if media:
+            file_type = attribute
+            break
 
-def get_file_properties(message: Message):
-    file_name = message.file.name
-    file_size = message.file.size or 0
-    mime_type = message.file.mime_type
+    if not media: abort(400, 'Unknown file type.')
+
+    file_name = getattr(media, 'file_name', None)
+    file_size = getattr(media, 'file_size', 0)
 
     if not file_name:
-        attributes = {
+        file_format = {
             'video': 'mp4',
             'audio': 'mp3',
             'voice': 'ogg',
             'photo': 'jpg',
             'video_note': 'mp4'
-        }
-
-        for attribute in attributes:
-            media = getattr(message, attribute, None)
-            if media:
-                file_type, file_format = attribute, attributes[attribute]
-                break
-        
-        if not media:
-            abort(400, 'Invalid media type.')
-
+        }.get(file_type)
         date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         file_name = f'{file_type}-{date}.{file_format}'
     
-    if not mime_type:
-        mime_type = guess_type(file_name)[0] or 'application/octet-stream'
-    
+    mime_type = guess_type(file_name)[0] or 'application/octet-stream'
+
     return file_name, file_size, mime_type
