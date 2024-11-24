78e2f63437ed198c5132c2ee78a19cf8431c8be2
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-11-24 16:07:48 +0530

    Bump to v1.8

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,7 @@
 from hydrogram import filters
 from hydrogram.types import Message
 from bot import TelegramBot
+from bot.config import Telegram
 from bot.modules.static import *
 from bot.modules.decorators import verify_user
 
@@ -8,11 +9,15 @@ from bot.modules.decorators import verify_user
 @verify_user
 async def start_command(_, msg: Message):
     await msg.reply(
-        text=WelcomeText % {'first_name': msg.from_user.first_name},
-        quote=True
+        text = WelcomeText % {'first_name': msg.from_user.first_name},
+        quote = True
     )
 
 @TelegramBot.on_message(filters.command('privacy') & filters.private)
 @verify_user
 async def privacy_command(_, msg: Message):
-    await msg.reply(text=PrivacyText, quote=True)
+    await msg.reply(text=PrivacyText, quote=True, disable_web_page_preview=True)
+
+@TelegramBot.on_message(filters.command('log') & filters.chat(Telegram.OWNER_ID))
+async def log_command(_, msg: Message):
+    await msg.reply_document('event-log.txt', quote=True)
