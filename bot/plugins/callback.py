29a7ba05bb5cb342f5c38a4b440f18a9f49b668a
Dr.Caduceus <GitHub@thecaduceus.eu.org>
2024-12-01 18:24:29 +0530

    Improve callback handling.

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1,4 +1,5 @@
 from hydrogram.types import CallbackQuery
+from hydrogram.errors import MessageDeleteForbidden
 from bot import TelegramBot
 from bot.modules.decorators import verify_user
 from bot.modules.static import *
@@ -6,26 +7,29 @@ from bot.modules.telegram import get_message
 
 @TelegramBot.on_callback_query()
 @verify_user
-async def manage_callback(bot, q: CallbackQuery):
-    query = q.data
+async def manage_callback(_, query: CallbackQuery):
+    query_data = query.data
 
-    if query.startswith('rm_'):
-        sq = query.split('_')
+    if query_data.startswith('rm_'):
+        splited_query_data = query_data.split('_')
 
-        if len(sq) != 3:
-            return await q.answer(InvalidQueryText, show_alert=True)
+        if len(splited_query_data) != 3:
+            return await query.answer(InvalidQueryText, show_alert=True)
 
-        message = await get_message(int(sq[1]))
+        message = await get_message(int(splited_query_data[1]))
 
         if not message:
-            return await q.answer(MessageNotExist, show_alert=True)
+            return await query.answer(MessageNotExist, show_alert=True)
         
-        sc = message.caption.split('/')
+        splited_caption = message.caption.split('/')
 
-        if q.from_user.id != int(sc[1]) or sq[2] != sc[0]:
-            return await q.answer(InvalidQueryText, show_alert=True)
+        if query.from_user.id != int(splited_caption[1]) or splited_query_data[2] != splited_caption[0]:
+            return await query.answer(InvalidQueryText, show_alert=True)
         
-        await message.delete()
-        await q.answer(LinkRevokedText, show_alert=True)
+        try:
+            await message.delete()
+            await query.answer(LinkRevokedText, show_alert=True)
+        except MessageDeleteForbidden: # Bot not have permission or File is older than 48 hours
+            await query.answer(FileDeleteForbiddenText, show_alert=True)
     else:
-        await q.answer(InvalidQueryText, show_alert=True)
+        await query.answer(InvalidQueryText, show_alert=True)
